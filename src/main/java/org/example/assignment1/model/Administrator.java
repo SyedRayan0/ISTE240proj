a0bc366c2f384a470977c56ba24e85297536aa06
Ali Faisal <af7751@rit.edu>
2026-02-20 11:35:26 +0400

    Independently added Administrator bean for event tracking

--- src/main/java/org/example/assignment1/model/Administrator.java
+++ src/main/java/org/example/assignment1/model/Administrator.java
@@ -1,4 +1,50 @@
 package org.example.assignment1.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Administrator {
+
+    private String adminId = "ADM-001";
+    private String accessLevel = "Full";
+    private String department = "Operations";
+
+    // Default Constructor
+    public Administrator() {
+    }
+
+    // Getters and Setters
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "adminId='" + adminId + '\'' +
+                ", accessLevel='" + accessLevel + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
 }
